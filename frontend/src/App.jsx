1269ddd36e43dabf8d4648f91cde7bcacfdebd3c
GrCOTE7 <grcote7@gmail.com>
2026-01-18 17:03:34 +0100

    better title (h1)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <div className="flex items-center justify-center min-h-screen bg-slate-50 text-slate-800 font-[Roboto]">
       <div className="max-w-4xl mx-auto p-8 text-center bg-white rounded-2xl shadow-sm">
         <h1 className="text-4xl font-bold mb-4 text-blue-600">
-          Frontend React
+          Frontend Matrice (React)
         </h1>
         <p className="text-lg mb-8 text-slate-600">
           Backend says:{" "}
